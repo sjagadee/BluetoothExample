833827a5bf5656c47638f802ae917a58befd13a0
sjagadee <sjagadee@hawk.iit.edu>
2017-05-30 18:37:04 -0500

    Created a button which helps us enable and disable the bluetooth via our application

--- app/src/main/java/com/example/srinivas/bluetoothexample/MainActivity.java
+++ app/src/main/java/com/example/srinivas/bluetoothexample/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.srinivas.bluetoothexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    BluetoothAdapter mBluetoothAdapter;
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "mBroadcastReceiver1: State OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "mBroadcastReceiver1: State Turing OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "mBroadcastReceiver1: State ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "mBroadcastReceiver1: State Turning ON");
+                        break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOnOrOff = (Button) findViewById(R.id.btnOnOrOff);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnOnOrOff.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Called enabling/ disabling button");
+                enableDisableBT();
+            }
+        });
+
+    }
+
+    /**
+     * This method would basically help us in switching on / switching off the bluetooth
+     *
+     * If the bluetooth is off - then this method would switch the bluetooth on
+     * If the bluetooth is on - then this method would switch the bluetooth off
+     *
+     * There are 3 scenarios in this method
+     */
+    private void enableDisableBT() {
+        if(mBluetoothAdapter == null) {
+            Log.d(TAG, "enableDisableBT: Does not have capabilities to on and off BT.");
+        }
+
+        // This means our bluetooth is not enabled Or off.
+        if(!mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: enabling BT.");
+            Intent enbleBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enbleBTIntent);
+
+            IntentFilter btIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, btIntentFilter);
+        }
+
+        // This means our bluetooth is enabled or On.
+        if(mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: disabling BT.");
+            mBluetoothAdapter.disable();
+
+            IntentFilter btIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, btIntentFilter);
+        }
+
     }
 }
